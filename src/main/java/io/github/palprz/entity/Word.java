0fbdeabf24d8f08756e3851775f417e79a719e4b
Przemyslaw Paluch <palprz@gmail.com>
2017-02-22 23:14:40 +0000

    Added basic entities and other little changes.
    - Updated README.md with lists of features
    - Added basic entities: Word and WordMap
    - Updated a little bit fxml file for testing showing list of entities

---
+++ src/main/java/io/github/palprz/entity/Word.java
@@ -0,0 +1,24 @@
+package io.github.palprz.entity;
+
+public class Word {
+
+	private Integer id;
+	private String name;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId( Integer id ) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName( String name ) {
+		this.name = name;
+	}
+
+}
---
+++ src/main/java/io/github/palprz/entity/WordMap.java
@@ -0,0 +1,33 @@
+package io.github.palprz.entity;
+
+public class WordMap {
+
+	private Integer id;
+	private String searchWord;
+	private String translation;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId( Integer id ) {
+		this.id = id;
+	}
+
+	public String getSearchWord() {
+		return searchWord;
+	}
+
+	public void setSearchWord( String searchWord ) {
+		this.searchWord = searchWord;
+	}
+
+	public String getTranslation() {
+		return translation;
+	}
+
+	public void setTranslation( String translation ) {
+		this.translation = translation;
+	}
+
+}
